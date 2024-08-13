6f1f15025993060cb6a056a130bc1877b3e6b288
hamid-manouchehri <hamid.manouchehri.njf@gmail.com>
2024-08-12 23:57:55 -0400

    manipulate images with hdf5

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Notes:
 1. Since data is not being tracked by the repo, wherever you clone the repository, create a `/data` directory including  two folders of `FT_csv_data` and `img_data`.
+
+2. Activate the conda environment while using python scripts: `conda activate CONDA_ENV_NAME`.
--- scripts/gelsight_mini_interface.py
+++ scripts/gelsight_mini_interface.py
@@ -35,42 +35,88 @@
 import cv2
 from os import getcwd
 from os.path import join, abspath 
-from gelsight import gsdevice
+import h5py
+import numpy as np
+import time
+from gelsight import gsdevice 
 
 
-img_name = 'img1.png'  # TODO
-
-current_dir = getcwd()
-parent_dir = join(current_dir, '..')  # Go one level up from the current_dir
-parent_dir_abs = abspath(parent_dir)
-dir_to_save_img = join(parent_dir_abs, 'data/img_data')
-
-
-def show_image(sensor):
-
-    while sensor.while_condition:
-
-        # get the roi image
-        f1 = sensor.get_image()
-        # if USE_ROI:
-        #     f1 = f1[int(roi[1]):int(roi[1] + roi[3]), int(roi[0]):int(roi[0] + roi[2])]
-        bigframe = cv2.resize(f1, (f1.shape[0]*2, f1.shape[1]*2))
-        cv2.imshow('Image', bigframe)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-
-def main():
+class GelsightMiniClass:
+    # Class attribute (shared by all instances of the class)
     sensor = gsdevice.Camera("GelSight Mini")
-
     sensor.connect()
-    # show_image(sensor)
 
-    sensor.get_image()
-    sensor.save_image(dir_to_save_img + '/' + img_name)
+    current_dir = getcwd()
+    parent_dir = join(current_dir, '..')  # Go one level up from the current_dir
+    parent_dir_abs = abspath(parent_dir)
+    dir_to_save_img = join(parent_dir_abs, 'data/img_data/')
+
+    def __init__(self, instance_attribute):
+        # Instance attribute (unique to each instance of the class)
+        self.instance_attribute = instance_attribute
+
+
+    # Instance method (can access instance attributes)
+    def save_png_image(cls, image_name='test_img.png'):
+
+        cls.sensor.get_image()
+        cls.sensor.save_image(cls.dir_to_save_img + image_name)
 
     
+    def show_image(cls):
+
+        while cls.sensor.while_condition:
+
+            f1 = cls.sensor.get_image()
+            bigframe = cv2.resize(f1, (f1.shape[0]*2, f1.shape[1]*2))
+            cv2.imshow('Image', bigframe)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+
+    def save_img_in_hdf5(cls, hdf5_file_name='test.h5'):
+
+        imgs=[]
+
+        for i in range(5):
+            img = cls.sensor.get_image()
+            imgs.append(img)
+
+        imgs = np.array(imgs)
+
+        with h5py.File(cls.dir_to_save_img + hdf5_file_name, 'w') as f:
+            f.create_dataset('images', data=imgs, compression='gzip', compression_opts=5)
+        
+
+
+    def save_hdf5_as_png(cls, hdf5_file_name='test.h5'):
+
+        with h5py.File(cls.dir_to_save_img + hdf5_file_name, 'r') as f:
+                img_array = f['images'][:] 
+
+        # print(np.shape(img_array))
+        # print(img_array)
+
+        for i, image_data in enumerate(img_array):
+            # Convert the image data to uint8 if necessary
+            image_data = np.array(image_data, dtype=np.uint8)
+
+            ## showing each image separately:
+            # cv2.imshow(f'Image {i+1}', image_data)
+            # print(f"Press any key to continue to the next image ({i+1}/{img_array.shape[0]})...")
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+
+            cls.save_png_image('hdf5_image_' + str(i) + '.png')
+
+
 
 if __name__ == '__main__':
-    main()
+
+    gelsight_mini_obj = GelsightMiniClass('hello')
+
+    # gelsight_mini_obj.save_img_in_hdf5()
+
+    gelsight_mini_obj.save_hdf5_as_png()
